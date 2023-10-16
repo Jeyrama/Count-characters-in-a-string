79069b451168e3fb5ee6de03c7111bd4118f7e2d
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-16 13:37:03 -0700

    Update Count-characters-in-a-string.js

--- Count-characters-in-a-string.js
+++ Count-characters-in-a-string.js
@@ -7,3 +7,11 @@ What if the string is empty? Then the result should be empty object literal, {}.
 
 
 // Solution
+
+function count(string) {
+  let count = {};
+  string.split('').forEach(function(s) {
+    count[s] ? count[s]++ : count[s] = 1;
+  });
+  return count;
+}

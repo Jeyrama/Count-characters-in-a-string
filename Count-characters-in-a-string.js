c5cf68904a018a9ba5ab661b8dc3a3dd904cb982
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-16 13:42:05 -0700

    Update Count-characters-in-a-string.js

--- Count-characters-in-a-string.js
+++ Count-characters-in-a-string.js
@@ -17,3 +17,16 @@ function count(string) {
 }
 
 // or
+
+function count(string) {
+  let result = {};
+
+  for(let i = 0; i < string.length; i++) {
+    if(result.hasOwnProperty(string[i])) {
+      result[string[i]] += 1;
+    } else {
+      result[string[i]] = 1;
+    }
+  }
+  return result;
+}
